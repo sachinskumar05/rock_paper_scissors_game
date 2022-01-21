ff2b199c8ca1d597c021fae284f0515ae8ad8cde
SK <sachin.skumar05@gmail.com>
2022-01-21 20:47:47 +0800

    First cut

---
+++ src/test/java/com/sachin/rps/RpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.rps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
