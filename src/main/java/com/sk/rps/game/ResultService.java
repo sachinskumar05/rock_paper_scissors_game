08ad4fc3fd539c5afb23165f0e1eae4e037c4e0e
SK <sachin.skumar05@gmail.com>
2022-02-06 11:47:45 +0800

    basic formatting

--- src/main/java/com/sk/rps/game/ResultService.java
+++ src/main/java/com/sk/rps/game/ResultService.java
@@ -11,16 +11,15 @@ import static com.sk.rps.game.CHOICES.*;
 @ComponentScan(value = "com.sk.rps")
 public class ResultService {
 
-    public RESULT execute(CHOICES humanChosen, CHOICES computerChosen) {
-        if ( humanChosen == computerChosen ) return RESULT.TIE;
-        switch (humanChosen) {
-            case ROCK:
-                return (computerChosen == SCISSORS ? RESULT.WON : RESULT.LOST);
-            case PAPER:
-                return (computerChosen == ROCK ? RESULT.WON : RESULT.LOST);
-            case SCISSORS:
-                return (computerChosen == PAPER ? RESULT.WON : RESULT.LOST);
-        }
-        return RESULT.TIE;
+  public RESULT execute(CHOICES humanChosen, CHOICES computerChosen) {
+    switch (humanChosen) {
+      case ROCK:
+        return (computerChosen == SCISSORS ? RESULT.WON : RESULT.LOST);
+      case PAPER:
+        return (computerChosen == ROCK ? RESULT.WON : RESULT.LOST);
+      case SCISSORS:
+        return (computerChosen == PAPER ? RESULT.WON : RESULT.LOST);
     }
+    return RESULT.TIE;
+  }
 }
